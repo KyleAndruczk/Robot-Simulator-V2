62f9824e86d556e136cf1cf6e559d67ca80501e4
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-07-22 15:00:48 -0400

    done

--- index.js
+++ index.js
@@ -3,4 +3,42 @@ document.addEventListener("DOMContentLoaded", function() {
   init();
 
   // ADD CODE HERE!
-});
+  const moves = document.querySelector("#moves-container")
+  const moveBtn = document.querySelector("#move-button")
+
+  document.addEventListener("keydown", (evnt) => {
+    const move = document.createElement('li')
+    if (evnt.key === "ArrowUp") {
+      move.textContent = "up"
+    }
+    else if (evnt.key === "ArrowDown") {
+      move.textContent = "down"
+    }
+    else if (evnt.key === "ArrowRight") {
+      move.textContent = "right"
+    }
+    else if (evnt.key === "ArrowLeft") {
+      move.textContent = "left"
+    }
+    moves.append(move)
+  })
+
+  moveBtn.addEventListener('click', (evnt) => {
+
+    
+    let timer = setInterval(() => {
+      const currMove = moves.querySelector('li')
+      if (currMove != null) {
+        move(currMove.textContent)
+        currMove.remove()
+      }
+      else {
+        clearInterval(timer)
+      }
+    }, 500)
+
+
+  })
+
+
+})
